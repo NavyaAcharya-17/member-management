0fbd68270dd898f511e0771b60956f4498ec6686
NavyaAcharya-17 <navyaleela17@gmail.com>
2025-10-17 21:22:21 +0530

    added in-memory caching

--- src/main/java/com/surest/member_management/service/impl/MemberServiceImpl.java
+++ src/main/java/com/surest/member_management/service/impl/MemberServiceImpl.java
@@ -10,9 +10,13 @@ import com.surest.member_management.repository.MemberRepository;
 import com.surest.member_management.service.MemberService;
 import com.surest.member_management.specification.MemberSpecification;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -24,6 +28,7 @@ public class MemberServiceImpl implements MemberService {
     private final MemberMapper memberMapper;
 
     @Override
+    @Transactional(readOnly = true)
     public Page<MemberResponse> getMembers(String firstName, String lastName, Pageable pageable) {
         return memberRepository.findAll(
                 MemberSpecification.filterBy(firstName, lastName),
@@ -32,6 +37,8 @@ public class MemberServiceImpl implements MemberService {
     }
 
     @Override
+    @Transactional(readOnly = true)
+    @Cacheable(value = "members", key = "#id")
     public MemberResponse getMemberById(UUID id) {
         Member member = memberRepository.findById(id)
                 .orElseThrow(() -> new MemberNotFoundException(id));
@@ -39,6 +46,7 @@ public class MemberServiceImpl implements MemberService {
     }
 
     @Override
+    @Transactional
     public MemberResponse createMember(MemberRequest request) {
         memberRepository.findByEmail(request.getEmail())
                 .ifPresent(existingMember -> {
@@ -50,6 +58,8 @@ public class MemberServiceImpl implements MemberService {
     }
 
     @Override
+    @Transactional
+    @CachePut(value = "members", key = "#memberId")
     public MemberResponse updateMember(UUID memberId, MemberRequest request) {
         Member existingMember = memberRepository.findById(memberId)
                 .orElseThrow(() -> new MemberNotFoundException(memberId));
@@ -59,6 +69,8 @@ public class MemberServiceImpl implements MemberService {
     }
 
     @Override
+    @Transactional
+    @CacheEvict(value = "members", key = "#memberId")
     public void deleteMember(UUID memberId) {
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new MemberNotFoundException(memberId));
